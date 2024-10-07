4c7131dc78d2f0802dec5873ea90a6ce89c64d16
Nursat Bakyt <itsnoorsat@MacBook-Air-Nursat.local>
2024-10-07 16:21:26 +0500

    first commit push project

---
+++ js/main.js
@@ -0,0 +1,183 @@
+// var
+// const 
+// let
+
+// let number = 5;
+
+// number = 8;
+
+// console.log(number);
+
+// string
+// boolean
+// number
+// bigint
+// symbol
+
+//alert("Hello World");
+// let name = prompt("What is your name?");
+// alert("Welcome, " + name);
+// let check = confirm("hello world");
+// console.log(check)
+
+// + - * / %
+// > < >= <= == != 
+
+// let number = 5;
+// let number2= "5";
+
+// console.log(number == number2);
+// console.log(number === number2);
+
+// let number = 5;
+
+// // if (number >= 5){
+// //     console.log("more than 5");
+// // }else {
+// //     console.log("less than 5");
+// // }
+
+// let number = 5;
+
+// if (number == 5 && number == 6){
+//     console.log("yes")
+// }
+
+
+// if (number == 5 || number == 6){
+//     console.log("yes")
+// }
+
+// let number;
+
+// console.log(number ?? 10);
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let  i = 0;
+
+// while (i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let number = 3;
+
+// switch (number){
+//     case 1: console.log('no');
+//     break;
+//     case 2: console.log('no');
+//     break;
+//     case 3: console.log('no');
+//     break;
+//     case 4: console.log('no');
+//     break;
+//     case 5: console.log('yes');
+//     break;
+// }
+
+// function sayHello(){
+//     console.log('hello world');
+// }
+
+// const sayHello = () => {
+//     console.log("hello world");
+// }
+// sayHello();
+
+// let number = 8;
+
+// console.log(number == 5 ? 'true' : 'false');
+
+// let obj = {
+//     name: 'Maksat',
+//     surname: 'Alikhan',
+//     age: 18,
+//     getName: function(){
+//         console.log(this.name);
+//     }
+// }
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+// let obj = {
+//     a: 5
+// }
+
+// //let obj2 = {...obj};
+// // let obj2 = Object.assign({}, obj);
+
+// obj2.a = 10;
+
+// console.log(obj.a);
+// console.log(obj2.a);
+
+// let obj = {
+//     name: 'Maksat',
+//     surname: 'Alikhan'
+// }
+
+// obj.age = 18;
+
+// delete obj.name;
+
+// console.log(obj)
+// let name = prompt("What is your name?");
+
+// // let str = `Hello World, ${name}`;
+// // console.log(str[7]);
+// // console.log(str.length);
+// // console.log(str.substring(5))
+// // console.log(str);
+
+
+// console.log(arr.filter(item => item > 10));
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 == 1){
+//         console.log(arr[i]);
+//     }
+// }
+
+// map
+// forEach
+
+// arr.map(item => {
+//     console.log(item);
+// })
+
+// push
+// pop
+// shift
+// unshift
+
+
+// arr.push(28);
+// arr.pop();
+// arr.unshift(28);
+// delete arr[3];
+
+// console.log(arr)
+
+// let arr = [3, 10, 15, 7, 12, 20];
+
+// console.log(arr.sort((a,b) => a-b));
+
+// let str = ["sdu", "kbtu", "med", "muit"];
+
+// // console.log(str.join('/'));
+
+// let names = 'Вася, Петя, Маша';
+
+// let arr = names.split(', ');
+
+// // console.log(arr);
+
+// let arr = [3, 10, 15, 7, 12, 20];
+
+// console.log(arr.reduce((total, item) => {
+//     return total += item;
+// }, 0))
